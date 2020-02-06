057ac98415af64ad4baf33a61c1aff4443830a9b
LucasFelicianoATeixeira <lucasfeliciano326@gmail.com>
2020-02-06 10:25:10 -0300

    alteraçao

---
+++ Gufi.sql
@@ -0,0 +1,39 @@
+-- Cria o banco de dados com o nome Gufi
+CREATE DATABASE Gufi;
+
+-- Define o banco de dados que será utilizado
+USE Gufi;
+
+--CRIAÇAO DAS TABELAS
+CREATE TABLE TipoUsuario (
+	IdTipoUsuario INT PRIMARY KEY IDENTITY,
+	TituloTipoUsuario VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE TipoEvento (
+	IdTipoEvento INT PRIMARY KEY IDENTITY,
+	TituloTipoEvento VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Instituicao (
+	IdInstituicao INT PRIMARY KEY IDENTITY,
+	CNPJ CHAR(14) NOT NULL UNIQUE,
+	NomeFantasia VARCHAR (255) NOT NULL UNIQUE,
+	Endereco VARCHAR (255) NOT NULL UNIQUE
+);
+
+CREATE TABLE Usuario (
+	IdUsuario INT PRIMARY KEY IDENTITY,
+	Nome VARCHAR(255) NOT NULL,
+	Email VARCHAR(255) NOT NULL UNIQUE,
+	Senha VARCHAR(255) NOT NULL,
+	DataCadastro DATETIME2,
+	Genero VARCHAR(255),
+	IdTipoUsuario INT FOREIGN KEY REFERENCES TipoUsuario(IdTipoUsuario)
+);
+
+CREATE TABLE Presenca (
+	IdPresenca INT PRIMARY KEY IDENTITY,
+	IdUsuario INT FOREIGN KEY REFERENCES Usuario(IdUsuario),
+	IdEvento INT FOREIGN KEY REFERENCES Evento(IdEvento)
+);
